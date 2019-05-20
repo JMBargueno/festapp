3882c32eb3a61361399243e0269b154bd544101c
JMBargueno <jmbarguenolopez@gmail.com>
2019-05-20 19:11:17 +0200

    Primera pruebas con javascript, volumen bajado e intentando poner links del menu en activo

--- FestAppv1/src/main/resources/static/js/scripts.js
+++ FestAppv1/src/main/resources/static/js/scripts.js
@@ -1,3 +1,33 @@
-var elemento = document.getElementById("index");
-    elemento.className += "active";
-    var ruta = document.URL;
+
+//Encontrar ruta actual
+var ruta = window.location.pathname;
+
+//alert(ruta);
+//Switch para darle el active a su correspondiente
+switch (ruta) {
+    case '/index':
+        var elemento = document.getElementById("index");
+        elemento.className += " active";
+        break;
+    case '/':
+        var elemento = document.getElementById("index");
+        elemento.className += " active";
+        break;
+    case '/admin/users':
+    var elemento = document.getElementById("adminzone");
+    elemento.className += " active";
+    break;
+    
+    case 'admin/users':
+    var element = document.getElementById("adminzone");
+    var elemento = document.getElementById("dropdownId");
+
+    elemento.className += " active";
+    element.className += " active";
+    break;
+}
+
+
+//Bajar volumen música de bienvenida
+var vid = document.getElementById("welcomes");
+vid.volume = 0.02;

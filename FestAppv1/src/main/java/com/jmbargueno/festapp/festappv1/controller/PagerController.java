5874b95a3a6c78928908b33a454f31f14e474975
JMBargueno <jmbarguenolopez@gmail.com>
2019-05-26 11:15:09 +0200

    Buscar/paginar en su controlador

--- FestAppv1/src/main/java/com/jmbargueno/festapp/festappv1/controller/PagerController.java
+++ FestAppv1/src/main/java/com/jmbargueno/festapp/festappv1/controller/PagerController.java
@@ -26,53 +26,53 @@ import com.jmbargueno.festapp.festappv1.service.ProductService;
 public class PagerController {
 
 
-	private static final int BUTTONS_TO_SHOW = 5;
-	private static final int INITIAL_PAGE = 0;
-	private static final int INITIAL_PAGE_SIZE = 10;
-	private static final int[] PAGE_SIZES = { 10, 20, 40 };
-
-	@Autowired
-	PartyTypeService partyTypeService;
-	
-	@Autowired
-	private ProductService productService;
-	
-	@GetMapping("/searchList")
-	public String showProductsPage(@RequestParam("pageSize") Optional<Integer> pageSize,
-            @RequestParam("page") Optional<Integer> page, @RequestParam("nombre") Optional<String> nombre, Model model) {
-		model.addAttribute("partiesList", partyTypeService.findAll());
-    	// Evalúa el tamaño de página. Si el parámetro es "nulo", devuelve
-    	// el tamaño de página inicial.
-        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
-        
-        // Calcula qué página se va a mostrar. Si el parámetro es "nulo" o menor
-        // que 0, se devuelve el valor inicial. De otro modo, se devuelve el valor
-        // del parámetro decrementado en 1.
-        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
-        
-        String evalNombre = nombre.orElse(null);
-        
-        Page<Product> products = null;
-        
-        if (evalNombre == null) {
-        	products = productService.findAllPageable(PageRequest.of(evalPage, evalPageSize));
-        } else {
-        	products = productService.findByNombreContainingIgnoreCasePageable(evalNombre, PageRequest.of(evalPage, evalPageSize));
-        }
-
-        // Obtenemos la página definida por evalPage y evalPageSize de objetos de nuestro modelo
-        //Page<Producto> products = productService.findAllPageable(PageRequest.of(evalPage, evalPageSize));
-        // Creamos el objeto Pager (paginador) indicando los valores correspondientes.
-        // Este sirve para que la plantilla sepa cuantas páginas hay en total, cuantos botones
-        // debe mostrar y cuál es el número de objetos a dibujar.
-        Pager pager = new Pager(products.getTotalPages(), products.getNumber(), BUTTONS_TO_SHOW);
-        
-        model.addAttribute("productos", products);
-        model.addAttribute("selectedPageSize", evalPageSize);
-        model.addAttribute("pageSizes", PAGE_SIZES);
-        model.addAttribute("pager", pager);
-        
-    	return "buy/listAll.html";
-    }
+//	private static final int BUTTONS_TO_SHOW = 5;
+//	private static final int INITIAL_PAGE = 0;
+//	private static final int INITIAL_PAGE_SIZE = 10;
+//	private static final int[] PAGE_SIZES = { 10, 20, 40 };
+//
+//	@Autowired
+//	PartyTypeService partyTypeService;
+//	
+//	@Autowired
+//	private ProductService productService;
+//	
+//	@GetMapping("/searchList")
+//	public String showProductsPage(@RequestParam("pageSize") Optional<Integer> pageSize,
+//            @RequestParam("page") Optional<Integer> page, @RequestParam("nombre") Optional<String> nombre, Model model) {
+//		model.addAttribute("partiesList", partyTypeService.findAll());
+//    	// Evalúa el tamaño de página. Si el parámetro es "nulo", devuelve
+//    	// el tamaño de página inicial.
+//        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
+//        
+//        // Calcula qué página se va a mostrar. Si el parámetro es "nulo" o menor
+//        // que 0, se devuelve el valor inicial. De otro modo, se devuelve el valor
+//        // del parámetro decrementado en 1.
+//        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
+//        
+//        String evalNombre = nombre.orElse(null);
+//        
+//        Page<Product> products = null;
+//        
+//        if (evalNombre == null) {
+//        	products = productService.findAllPageable(PageRequest.of(evalPage, evalPageSize));
+//        } else {
+//        	products = productService.findByNombreContainingIgnoreCasePageable(evalNombre, PageRequest.of(evalPage, evalPageSize));
+//        }
+//
+//        // Obtenemos la página definida por evalPage y evalPageSize de objetos de nuestro modelo
+//        //Page<Producto> products = productService.findAllPageable(PageRequest.of(evalPage, evalPageSize));
+//        // Creamos el objeto Pager (paginador) indicando los valores correspondientes.
+//        // Este sirve para que la plantilla sepa cuantas páginas hay en total, cuantos botones
+//        // debe mostrar y cuál es el número de objetos a dibujar.
+//        Pager pager = new Pager(products.getTotalPages(), products.getNumber(), BUTTONS_TO_SHOW);
+//        
+//        model.addAttribute("productos", products);
+//        model.addAttribute("selectedPageSize", evalPageSize);
+//        model.addAttribute("pageSizes", PAGE_SIZES);
+//        model.addAttribute("pager", pager);
+//        
+//    	return "buy/listAll.html";
+//    }
 	
 }
